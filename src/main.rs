7d4ce5d8a850dc6b3b325888d96d3939ed80ea56
Mia Haidar Graziano Carvalho <mateus.haidar@icloud.com>
2023-11-22 08:39:34 -0300

    ?

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,48 @@ use std::io;
 
 fn main() {
     loop {
-        println!("Type a number 1 for converting F to C and 2 for converting C to F");
-        let mut input = String::new();
+        println!("Please select the conversion type: (type 'exit' to exit program)");
+        println!("1. Fahrenheit to Celsius");
+        println!("2. Celsius to Fahrenheit");
 
+        let mut conversion_type = String::new();
         io::stdin()
-            .read_line(&mut input)
-            .expect("failed to read line");
-
-        let input: i32 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+            .read_line(&mut conversion_type)
+            .expect("Failed to read line");
+        let conversion_type = conversion_type.trim();
+        let conversion_type = match conversion_type {
+            "1" => 1,
+            "2" => 2,
+            "exit" => break,
+            _ => {
+                println!("Please input 1 or 2!");
+                continue;
+            }
         };
 
-        if input == 1 {
-            let mut input = String::new();
-            println!("Type a temperature in fahrenheit");
-            io::stdin()
-                .read_line(&mut input)
-                .expect("failed to read line");
+        println!("Please input the temperature:");
+        let mut temperature = String::new();
+        io::stdin()
+            .read_line(&mut temperature)
+            .expect("Failed to read line");
 
-            let input: f64 = match input.trim().parse() {
-                Ok(num) => num,
-                Err(_) => continue,
-            };
-
-            let output = calculate_celsius(input);
-            println!("{input} fahrenheit in celsius is: {output}");
+        if temperature.trim() == "exit" {
             break;
-        } else if input == 2 {
-            let mut input = String::new();
-            println!("Type a temperature in celsius");
-
-            io::stdin()
-                .read_line(&mut input)
-                .expect("failed to read line");
-
-            let input: f64 = match input.trim().parse() {
-                Ok(num) => num,
-                Err(_) => continue,
-            };
-
-            let output = calculate_fahrenheit(input);
-            println!("{input} celsius in fahrenheit is: {output}");
-            break;
-        } else {
-            println!("Try again and type a valid number!!");
         }
+
+        let temperature: f64 = match temperature.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please input a valid temperature");
+                continue;
+            }
+        };
+
+        let converted_temperature = if conversion_type == 1 {
+            (temperature - 32.) / 1.8
+        } else {
+            temperature * 1.8 + 32.
+        };
+        println!("The converted temperature is {}", converted_temperature);
     }
-}
-
-fn calculate_fahrenheit(x: f64) -> f64 {
-    x * (9.0 / 5.0) + 32.0
-}
-
-fn calculate_celsius(x: f64) -> f64 {
-    5.0 / 9.0 * (x - 32.0)
 }
